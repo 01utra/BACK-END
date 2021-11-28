250eb1d5c6fb4c5a17aff278a8a00aec33e75d50
Bruno <obruno26@outlook.com>
2021-11-28 12:13:46 -0300

    iniciação do projeto, configuração do banco de dados e criação da classe model

---
+++ src/main/java/br/com/provi/utra/UtraApplication.java
@@ -0,0 +1,13 @@
+package br.com.provi.utra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/provi/utra/UtraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.provi.utra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
